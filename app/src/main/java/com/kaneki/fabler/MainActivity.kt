69ef93ed2ecf93c76fbf9f7ac96854737a4eb8e4
onyasumi <boku@kaneki.dev>
2024-03-06 05:53:56 -0500

    Create Top Bar

--- app/src/main/java/com/kaneki/fabler/MainActivity.kt
+++ app/src/main/java/com/kaneki/fabler/MainActivity.kt
@@ -3,65 +3,19 @@ package com.kaneki.fabler
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.UiComposable
-import androidx.compose.ui.tooling.preview.Preview
+import com.kaneki.fabler.ui.header.Header
 import com.kaneki.fabler.ui.theme.FablerTheme
-import androidx.compose.material3.Tab
-import androidx.compose.material3.TabRow
-import androidx.compose.material3.Scaffold
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             FablerTheme {
-                // A surface container using the 'background' color from the theme
-                MainContent()
+
+                Header()
+
+
             }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FablerTheme {
-        Greeting("Android")
-    }
-}
-
-@Composable
-fun MainContent() {
-    Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-        Column() {
-            Greeting("Android")
         }
     }
 }
--- app/src/main/java/com/kaneki/fabler/ui/header/header.kt
+++ app/src/main/java/com/kaneki/fabler/ui/header/header.kt
@@ -1,2 +1,57 @@
 package com.kaneki.fabler.ui.header
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.kaneki.fabler.R
+
+@Preview
+@Composable
+fun Header() {
+
+    Surface(Modifier.fillMaxWidth()) {
+
+        Row(verticalAlignment = Alignment.CenterVertically) {
+
+            Image(
+                painter = painterResource(id = R.drawable.search),
+                contentDescription = "Search",
+                colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.secondary),
+                modifier = Modifier.padding(10.dp)
+            )
+
+            Spacer(Modifier.weight(1f))
+
+            Text("Fabler", fontSize = 24.sp)
+
+            Spacer(Modifier.weight(1f))
+
+            Image(
+                painter = painterResource(id = R.drawable.user),
+                contentDescription = "User",
+                colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.secondary),
+                modifier = Modifier.padding(10.dp)
+            )
+
+        }
+
+    }
+
+}
