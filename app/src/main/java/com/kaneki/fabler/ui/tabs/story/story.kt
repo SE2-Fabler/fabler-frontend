721047a2740961deccbbd8827b027f0ecd0a1d79
Eric Wang <e74wang@uwaterloo.ca>
2024-03-06 06:40:53 -0500

    update story UI

--- app/src/main/java/com/kaneki/fabler/ui/tabs/story/story.kt
+++ app/src/main/java/com/kaneki/fabler/ui/tabs/story/story.kt
@@ -1,6 +1,7 @@
 package com.kaneki.fabler.ui.tabs.story
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
@@ -10,7 +11,13 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -21,11 +28,15 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
 import androidx.compose.ui.window.Popup
 import androidx.compose.ui.window.PopupProperties
+import com.kaneki.fabler.R
+import com.kaneki.fabler.ui.tabs.user.UserEntry
+import com.kaneki.fabler.ui.tabs.user.userList
 
 /*
 @Composable
@@ -64,32 +75,42 @@ fun SimpleTabLayout() {
     )
 }*/
 
-class Story(val title: String, val author: String, val body: String){
+class Story(val title: String, val author: String, val body: String, val profileImageResId: Int){}
 
-}
-
+val storyList = listOf(
+    Story(
+        title = "title 1", author = "test author",
+        body = "this is the content of a story",
+        R.drawable.fable1
+    ),
+    Story(
+        title = "title 2", author = "test author",
+        body = "this is the content of a story",
+        R.drawable.fable2
+    ),
+    Story(
+        title = "title 3", author = "test author",
+        body = "this is the content of a story",
+        R.drawable.fable2
+    )
+)
 
 @Composable
 @Preview(showBackground = true)
 fun StoryScreen() {
     Column(
         modifier = Modifier
-            .fillMaxSize(),
+            .fillMaxSize()
+            .padding(16.dp)
         //verticalArrangement = Arrangement.Top,
         //horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        StoryEntry(
-            Story(
-                title = "title 1", author = "test author",
-                body = "this is the content of a story"
-            )
-        )
-        StoryEntry(
-            Story(
-                title = "title 2", author = "test author",
-                body = "this is the content of a story"
-            )
-        )
+        storyList.forEach { story ->
+            Spacer(modifier = Modifier.height(8.dp))
+            StoryEntry(story)
+            Spacer(modifier = Modifier.height(8.dp))
+            Divider()
+        }
     }
 }
 
@@ -140,20 +161,33 @@ fun StoryPopup(content: Story, showPopup: Boolean, onClickOutside: () -> Unit) {
 @Composable
 fun StoryEntry(content: Story) {
     var showPopup by rememberSaveable { mutableStateOf(false) }
-    Column(
+    Row(
         modifier = Modifier
-            .fillMaxWidth()
-            .border(BorderStroke(1.dp, Color.Black))
-            .clickable {
-                showPopup = true
-            }
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
     ) {
-        Text(
-            text = content.title
+        Image(
+            painter = painterResource(id = content.profileImageResId),
+            contentDescription = "Story image",
+            modifier = Modifier
+                .size(64.dp)
+                .clip(CircleShape)
         )
-        Text(
-            text = "By: ${content.author}"
-        )
+        Spacer(modifier = Modifier.width(16.dp))
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .clickable {
+                    showPopup = true
+                }
+        ) {
+            Text(
+                text = content.title
+            )
+            Text(
+                text = "By: ${content.author}"
+            )
+        }
     }
     StoryPopup(content = content, showPopup = showPopup, onClickOutside = {showPopup = false})
 }
